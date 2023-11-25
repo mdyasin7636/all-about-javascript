c36d86a9fef57cda9538d6bfe6999583501c84b2
mdyasin7636 <mdyasin7636@gmail.com>
2023-11-25 11:49:10 +0600

    updated

---
+++ parse.js
@@ -0,0 +1,6 @@
+const num1 = 1.2
+const num2 = 1.3
+
+const total = num1 + num2
+
+console.log(parseInt(total));
---
+++ type.js
@@ -0,0 +1,12 @@
+var isString = 'rahim'
+console.log(typeof isString);
+
+var number = 123
+console.log(typeof number);
+
+var notNumber = '123'
+console.log(typeof notNumber);
+
+var isBoolean = true
+console.log(typeof isBolean);
+
