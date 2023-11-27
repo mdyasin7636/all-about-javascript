d98a1041942ee683d2818ea2c4f9edc7c48992e8
mdyasin7636 <mdyasin7636@gmail.com>
2023-11-27 15:36:35 +0600

    updated

---
+++ numbers.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(1101, 2)
+console.log(num1);
+
+let num2 = parseFloat("2.5e3")
+console.log(num2);
+
---
+++ shorthand.js
@@ -0,0 +1,20 @@
+let price1 = 100;
+let price2 = 100;
+let price3 = 200;
+let price4 = 300;
+let price5 = 2;
+let price6 = 3;
+
+price1 += 400;
+price2 -= 50;
+price3 /= 5;
+price4 *= 5;
+price5 ++;
+price6 --;
+
+console.log(price1);
+console.log(price2);
+console.log(price3);
+console.log(price4);
+console.log(price5);
+console.log(price6);
