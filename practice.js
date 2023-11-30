29781254c7903c74483a4f2d55422335a3dccfee
mdyasin7636 <mdyasin7636@gmail.com>
2023-11-30 15:12:30 +0600

    updated

---
+++ practice.js
@@ -0,0 +1,33 @@
+// let moneyGiven = 1000;
+
+// let costOfFruit = 700;
+
+// let remaining = moneyGiven - costOfFruit;
+
+// console.log(remaining);
+
+
+// let mathematics = 75.25;
+// let biology = 65;
+// let chemistry = 80;
+// let physics = 35.45;
+// let bangla = 99.50;
+
+// let totalNumbers = mathematics + biology + chemistry + physics + bangla
+
+// let averageMarks = totalNumbers / 5;
+// let finalResult = averageMarks.toFixed(2)
+
+// console.log(parseFloat(finalResult));
+
+ 
+// let firstLine = "I am going to be";
+// let secondLine = "an awesome web developer";
+
+// let finalOutput = firstLine + " " + secondLine;
+
+// console.log(finalOutput);
+
+
+
+
